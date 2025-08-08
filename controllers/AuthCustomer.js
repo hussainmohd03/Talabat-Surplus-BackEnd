3aa1f28abe5470820eba8b4309d767201b1d4402
hussainmohd03 <hussainmohdali03@gmail.com>
2025-08-08 15:19:50 +0300

    create customer login controller

--- controllers/AuthCustomer.js
+++ controllers/AuthCustomer.js
@@ -3,17 +3,23 @@ const middleWares = require('../middlewares')
 
 exports.Register = async (req, res) => {
   try {
+    const { email, password, first_name, last_name } = req.body
     let userInDB = await Customer.findOne({ email })
 
     if (userInDB) {
       return res.send(400).send('user with this email already exists')
     }
 
-    let passwordDigest = await middleWares.hashPassword(req.body.password)
+    let passwordDigest = await middleWares.hashPassword(password)
 
-    req.body.password = passwordDigest
+    const data = {
+      first_name: first_name,
+      last_name: last_name,
+      email: email,
+      password_digest: passwordDigest
+    }
 
-    const newUser = await Customer.create(req.body)
+    const newUser = await Customer.create(data)
 
     res.status(200).send(newUser)
   } catch (error) {
@@ -22,5 +28,26 @@ exports.Register = async (req, res) => {
 }
 
 exports.Login = async (req, res) => {
-  
+  try {
+    const { email, password } = req.body
+
+    const userInDB = await Customer.findOne({ email })
+
+    let matched = await middleWares.comparePassword(
+      password,
+      userInDB.password_digest
+    )
+    if (matched) {
+      const payload = {
+        id: userInDB._id,
+        email: userInDB.email,
+        role: 'customer'
+      }
+      let token = middleWares.createToken(payload)
+      return res.status(200).send({ user: payload, token })
+    }
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized' })
+  } catch (error) {
+    throw error
+  }
 }
